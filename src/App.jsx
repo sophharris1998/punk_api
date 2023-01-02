4431532ff5c687947aeca647a6b0c97a3a02781d
sophharris1998 <117087805+sophharris1998@users.noreply.github.com>
2023-01-02 17:41:37 +0000

    beers displaying

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.scss";
 import Card from "./Components/Card";
 
@@ -6,17 +6,35 @@ const App = () => {
   const [beers, setBeers] = useState([]);
 
   const getBeer = async () => {
-    const response = await fetch("https://api.punkapi.com/v2/beers");
+    const response = await fetch(
+      "https://api.punkapi.com/v2/beers?per_page=80"
+    );
     const data = await response.json();
-    console.log(data);
+
+    setBeers(data);
+    console.log(beers);
   };
 
-  getBeer();
+  useEffect(() => {
+    // 👇️ only runs once
+    console.log("useEffect ran");
+    getBeer();
+  }, []);
+
+  const beersCardJSX = beers.map((indiBeer) => {
+    return (
+      <Card
+        name={indiBeer.name}
+        image={indiBeer.image_url}
+        abv={indiBeer.abv}
+        first_brewed={indiBeer.first_brewed}
+      />
+    );
+  });
   return (
     <div>
       <h1> Punk API </h1>
-
-      <Card />
+      {beersCardJSX}
     </div>
   );
 };
