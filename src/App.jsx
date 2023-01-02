d3bcff53bf7bb0fb2143d9cdc41a3a59460dff89
sophharris1998 <117087805+sophharris1998@users.noreply.github.com>
2023-01-02 14:39:35 +0000

    beers console log

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import "./App.scss";
 
 const App = () => {
+  const getBeer = async () => {
+    const response = await fetch("https://api.punkapi.com/v2/beers");
+    const data = await response.json();
+    console.log(data);
+  };
+
+  getBeer();
   return (
     <div>
       <h1> Punk API </h1>
